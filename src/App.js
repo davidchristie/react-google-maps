667e1335d26a543913b8dcb21755dbf986aff06d
David Christie <davidchristie.nz@gmail.com>
2017-10-16 10:16:50 +1300

    Update app component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import React, { Component } from 'react'
 import Map from './components/Map'
 import Marker from './components/Marker'
 import './App.css'
-import logo from './logo.svg'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <img alt='logo' className='App-logo' src={logo} />
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>React Google Maps</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Map
           center={{lat: -25.363, lng: 131.044}}
           zoom={4}
